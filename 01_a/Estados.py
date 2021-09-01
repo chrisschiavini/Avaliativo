2a07b42db90c0d9befefead2cfbdfd7557c8b565
Christian <christianschiavini@outlook.com>
2021-09-01 20:43:06 -0300

    Vini e Hiuri commit

--- 01_a/Estados.py
+++ 01_a/Estados.py
@@ -1,7 +1,7 @@
 class Unidade_Federativa:
     @staticmethod
     def get_lista_uf():
-        v_estados_ufs = ["Acre * AC ","Amazonas * AM ","Amapá * AP ", "Pará * PA ", "Rondônia * RO ", "Roraima * RO ", "Tocantins * TO ","Alagoas  * AL ", "Bahia * BA ",
+        v_estados_ufs = ["Acre * AC ","Amazonas * AM ","Amapá * AP ", "Pará * PA ", "Rondônia * RO ", "Roraima * RR ", "Tocantins * TO ","Alagoas  * AL ", "Bahia * BA ",
                          "Ceará * CE ", "Maranhão * MA ", "Paraíba * PB ", "Pernambuco * PE ", "Piauí * PI ", "Rio Grande do Norte * RN ", "Sergipe * SE ",
                         "Paraná * PR ", "Rio Grande do sul * RS ", "Santa Catarina * SC ", "Espirito Santo * ES ", "Minas Gerais * MG ", "Rio de Janeiro * RJ ",
                          "São Paulo * SP ", "Goiás * GO ", "Mato Grosso * MT ", "Mato Grosso do Sul * MS ", "Distrito Federal * DF"]
